d4eb71d94d91f0bf7c8e278880770d38acd86db5
Nicolas Torres <desmondgamesyt@gmail.com>
2023-11-05 19:44:15 -0300

    added function to validate sign up data

--- utils/utils.ts
+++ utils/utils.ts
@@ -9,3 +9,37 @@ export function throwCustomError(message: string = httpStatus.http_message_inter
     throw customError;
 }
 
+export function validateSignUpData(name: string, lastname: string, username: string, password: string):void {
+    if (!name || !lastname || !username || !password) {
+        var fieldsMissing = [!name ? 'Name' : '', !lastname ? 'Last name' : '', !username ? 'Email' : '', !password ? 'Password' : ''].filter(Boolean).join(', ');
+        throwCustomError('The following fields are missing: ' + fieldsMissing, httpStatus.http_status_bad_request)
+    }
+    if (!validator.isEmail(username)) {
+        throwCustomError('You\'ve to enter a valid email', httpStatus.http_status_bad_request)
+    }
+    if (!validator.isAlpha(name) || !validator.isAlpha(lastname)) {
+        var fieldsNotAlpha = [!name ? 'name' : '', !lastname ? 'Last name' : ''].filter(Boolean).join('and ');
+        throwCustomError('Your ' + fieldsNotAlpha + ' must only contain letters', httpStatus.http_status_bad_request)
+    }
+    if (name.length > 100 || lastname.length > 100) {
+        var fieldsNotMaxLength = [!name ? 'name' : '', !lastname ? 'Last name' : ''].filter(Boolean).join('and ');
+        throwCustomError('Your ' + fieldsNotMaxLength + ' must be a maximum of 100 characters long.')
+    }
+    if (password.length < 8) {
+        throwCustomError('Your password must be at least 8 characters long.', httpStatus.http_status_bad_request);
+    }
+
+    if (password.length > 100) {
+        throwCustomError('Your password must be a maximum of 100 characters long.', httpStatus.http_status_bad_request);
+    }
+    if (!validator.isStrongPassword(password, {
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+        returnScore: false
+    })) {
+        throwCustomError('Your password is not strong enough', httpStatus.http_status_bad_request);
+    }
+}
