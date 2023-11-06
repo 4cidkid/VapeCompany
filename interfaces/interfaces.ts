230485a49938aea6774d23cfce00e9803d8d37c3
Nicolas Torres <desmondgamesyt@gmail.com>
2023-11-06 01:18:49 -0300

    added interfaces for next auth

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,6 +1,30 @@
+import NextAuth from "next-auth/next"
 export interface SignUpBody {
     name: string,
     lastname: string
     username: string, //email
     password: string
+}
+
+declare module "next-auth"{
+    interface User{
+        email:string,
+        name: string,
+        lastname: string
+        points: number
+    }
+    interface Session{
+        user:User & {
+            email: string,
+            name: string,
+            lastname: string
+            points: number
+        }
+        token:{
+            email: string
+            name: string,
+            lastname: string
+            points: number
+        }
+    }
 }
