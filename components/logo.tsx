0b731408a71ef65c8a25512d605e1b81b7430a6a
Nicolas Torres <desmondgamesyt@gmail.com>
2023-11-09 17:10:02 -0300

    added logo component

---
+++ components/logo.tsx
@@ -0,0 +1,14 @@
+import { twMerge } from "tailwind-merge"
+import Link from "next/link"
+import { Inter } from "next/font/google"
+import { clsx } from 'clsx';
+const inter = Inter({ subsets: ["latin"] })
+
+export default function Logo({ className }: { className?: string }) {
+    const classname = twMerge("w-10 h-10 text-2xl text-white font-bold", className ?? "")
+    return (
+        <Link href={"/"} className={clsx(classname, inter.className)}>
+            VapeCompany
+        </Link>
+    )
+}
