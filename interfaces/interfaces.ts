8232132176d029c5e71defce48cc7e1c9c078a59
Nicolas Torres <desmondgamesyt@gmail.com>
2023-11-10 17:56:10 -0300

    added interfaces for sign up

--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from "react";
+import { Toast } from "react-hot-toast";
 export interface SignUpBody {
     name: string,
     lastname: string
@@ -5,3 +7,59 @@ export interface SignUpBody {
     password: string
 }
 
+export interface StepTextProps {
+    text: string;
+    ref?: React.Ref<HTMLSpanElement>;
+}
+export interface StepsState {
+    stepOne: boolean,
+    stepTwo: boolean,
+    stepThree: boolean
+}
+export interface StepsProps {
+    step: number,
+    stepHover: StepsState,
+    setStepHover: Dispatch<SetStateAction<StepsState>>
+}
+export interface PasswordReq {
+    height: boolean,
+    lower: boolean,
+    upper: boolean,
+    number: boolean,
+    symbol: boolean
+}
+export interface StepOneProps {
+    email: string,
+    setEmail: Dispatch<SetStateAction<string>>,
+    password: string
+    setPassword: Dispatch<SetStateAction<string>>,
+    passwordReq: PasswordReq
+    showPasswordReq: boolean
+    setShowPasswordReq: Dispatch<SetStateAction<boolean>>,
+    setStep: Dispatch<SetStateAction<number>>
+}
+export interface StepTwoProps {
+    name: string,
+    lastName: string,
+    password: string,
+    email:string,
+    setName: Dispatch<SetStateAction<string>>,
+    setLastName: Dispatch<SetStateAction<string>>,
+    setStep: Dispatch<SetStateAction<number>>
+}
+export interface SendUserInfoArgs {
+    name: string
+    lastname: string
+    email: string
+    password: string,
+}
+export interface ApiFetchCommonReturn {
+    state: boolean,
+    message: string
+}
+export interface StepThreeProps {
+    token: string,
+    email:string
+    setToken: Dispatch<SetStateAction<string>>,
+    setRedirectUser: Dispatch<SetStateAction<boolean>>
+}
