45f265fd6e7d5022f8188c547b56bc38014c8521
Nicolas Torres <desmondgamesyt@gmail.com>
2023-11-09 21:54:42 -0300

    added sign in page for credentials & oath

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image"
 import Logo from "@/components/logo"
 import Link from "next/link"
-import SignInButton from "@/components/signInButton"
-export default function SignUp() {
+import SignInButton from "@/components/sign-in/signInButton"
+import SignInForm from "@/components/sign-in/signInForm"
+export default function SignIn() {
     return (
         <>
             <section className="flex items-center text-text">
@@ -18,22 +19,7 @@ export default function SignUp() {
 
                         <h1 className="text-black font-bold text-3xl capitalize mx-auto mb-8 text-center ">Sign in to your account</h1>
                         <main>
-                            <form aria-aria-label="sign in form" className="flex flex-col gap-4 mx-auto w-full child:flex child:flex-col child:gap-1">
-                                <div>
-                                    <label htmlFor="email" className=" font-extralight">Email address</label>
-                                    <input placeholder="Enter your email" type="email" name="email" id="email" className="focus-visible:outline-primary-400 pl-2 w-full border border-[rgba(0,0,0,0.27)] rounded-md py-2 shadow-sm" />
-                                </div>
-                                <div className="relative">
-                                    <div className="flex items-center justify-between w-full">
-                                        <label htmlFor="password" className="font-extralight">Password</label>
-                                        <Link className="text-primary-500" href="/forgot-password">Forgot password?</Link>
-                                    </div>
-                                    <input placeholder="Enter your email" type="password" name="password" id="password" className="focus-visible:outline-primary-400 pl-2 w-full border border-[rgba(0,0,0,0.27)] rounded-md py-2 shadow-sm" />
-                                </div>
-                                <button type="submit" className="mt-3 flex items-center justify-center py-2 bg-primary-500 hover:bg-primary-600 transition-colors  w-full text-lg font-medium text-white rounded-md shadow-sm">
-                                    Sign in
-                                </button>
-                            </form>
+                            <SignInForm />
                         </main>
                         <div className="flex flex-col gap-6  w-full ">
                             <div className="whitespace-nowrap w-full  mx-auto flex items-center gap-3 mt-4">
