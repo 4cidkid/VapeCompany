08381ce620685870b30593564522db9fadb7a391
Nicolas Torres <desmondgamesyt@gmail.com>
2023-11-15 19:36:58 -0300

    Update imports and add Categories component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
-import Header from '@/components/Header'
+import Header from '@/components/layout/Header'
 import HeroSection from '@/components/Hero'
 import Image from 'next/image'
-
+import Categories from '@/components/Categories'
+import { categories } from '@/mocks/search'
 export default function Home() {
+
   return (
     <>
       <Header />
-      <HeroSection/>
+      <HeroSection />
+      <Categories categories={categories} />
     </>
   )
 }
