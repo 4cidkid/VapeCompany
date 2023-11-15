dc1c10529fbee5ba3cceca30605ad005e87b6af2
Nicolas Torres <desmondgamesyt@gmail.com>
2023-11-15 19:36:35 -0300

    Add categories to searchProducts mock

--- mocks/search.ts
+++ mocks/search.ts
@@ -21,3 +21,23 @@ export const searchProducts = [
     'Coil Z6',
     'Vaper X7'
 ];
+export const categories = [{
+    name: 'Vape KITS',
+    image: 'https://cdn.nicotordev.com/files/02c5ca02-a321-4702-9c32-9fee5c2dcdcd.webp'
+},
+{
+    name: "Dispensables",
+    image: "https://cdn.nicotordev.com/files/317554f1-7f35-4550-94e2-eae3126646d2.webp"
+},
+{
+    name: "E-Liquids",
+    image: "https://cdn.nicotordev.com/files/b62a02d6-7a09-45e5-9d75-5e472839ad7a.webp"
+},
+{
+    name: "Pods & Tanks",
+    image: "https://cdn.nicotordev.com/files/24b43ed8-8f9a-49f2-943c-a379e25b6dfc.webp"
+},
+{
+    name: "Coils",
+    image: "https://cdn.nicotordev.com/files/c949dece-ead1-4b03-af0c-436fbc34d979.webp"
+}]
