91a025601caf0a94c5f8f541efd5ddaa5bdbea5e
Nicolas Torres <desmondgamesyt@gmail.com>
2023-11-14 21:42:17 -0300

    added a mockup to search products

---
+++ mocks/search.ts
@@ -0,0 +1,23 @@
+
+export const searchProducts = [
+    'Vaper X1',
+    'Vaper X2',
+    'Vaper X3',
+    'Juice Strawberry',
+    'Juice Mango',
+    'Juice Mint',
+    'Coil Z1',
+    'Coil Z2',
+    'Coil Z3',
+    'Vaper X4',
+    'Juice Apple',
+    'Juice Cherry',
+    'Coil Z4',
+    'Vaper X5',
+    'Juice Vanilla',
+    'Coil Z5',
+    'Vaper X6',
+    'Juice Chocolate',
+    'Coil Z6',
+    'Vaper X7'
+];
