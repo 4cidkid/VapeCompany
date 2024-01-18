7a5c9f3ac8a97fb7276b866351582fdef0fbea27
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-17 22:52:08 -0300

    Add newsletter section to home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,36 @@ import HeroSection from '@/components/home/Hero'
 import Image from 'next/image'
 import Categories from '@/components/home/Categories'
 import { categories } from '@/mocks/search'
+import Newsletter from '@/components/home/newsLetter'
+import vapeOneImage from '@/public/assets/images/home/vape-1.webp'
+import vapeTwoImage from '@/public/assets/images/home/vape-2.webp'
+import vapeThreeImage from '@/public/assets/images/home/vape-3.webp'
+import vapeFourImage from '@/public/assets/images/home/vape-4.webp'
 export default function Home() {
   const allCategories = categories
   return (
     <>
       <HeroSection />
-      <Categories categories={allCategories} numPagesProp={Math.round(categories.length / 6)} />
+      <Categories categories={allCategories} />
+      <div className='relative bg-primary-600 h-44 w-full my-16 py-4 flex items-center justify-center overflow-hidden'>
+        <div className='absolute w-full h-full z-10'>
+          <Image className='absolute left-2 top-2' width={400} height={400} src={vapeOneImage} alt='vape' />
+          <Image className='absolute left-12 -top-6' width={400} height={400} src={vapeTwoImage} alt='vape' />
+        </div>
+        <div className='flex flex-col justify-center gap-4 text-center relative z-20' >
+          <div className='gap-1'>
+            <b className='text-white text-3xl font-bold'>Discover Vape Bliss: Your Gateway to Flavorful Moments</b>
+            <p className='text-white'>Elevate Your Vaping Experience with Our Exclusive Newsletter</p>
+          </div>
+          <div className='flex items-stretch w-3/4 mx-auto gap-2'>
+            <Newsletter />
+          </div>
+        </div>
+        <div className='absolute w-full h-full z-10'>
+          <Image className='absolute right-2 -top-1' width={400} height={400} src={vapeThreeImage} alt='vape' />
+          <Image className='absolute right-12 -top-4' width={400} height={400} src={vapeFourImage} alt='vape' />
+        </div>
+      </div>
     </>
   )
 }
