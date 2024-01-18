e5e2594c719859bc789437027e455888689540e2
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-18 19:07:59 -0300

    Update link font size in Products component

--- components/home/products.tsx
+++ components/home/products.tsx
@@ -22,7 +22,7 @@ export default function Products() {
                 <div className="w-full">
                     <div className="pr-16 flex justify-between items-center mb-8">
                         <b className="text-3xl font-black">Deals of the week - <span className="text-[#8D8B8B]"> Enjoy the offer!</span></b>
-                        <Link href="/products" className="flex items-center gap-2 text-primary-500">
+                        <Link href="/products" className="flex items-center gap-2 text-primary-500 text-lg">
                             See all Deals of the week <FaArrowRight />
                         </Link>
                     </div>
@@ -33,7 +33,7 @@ export default function Products() {
                 <div className="w-full">
                     <div className="pr-16 flex justify-between items-center mb-8">
                         <b className="text-3xl font-black">Vape Kits - <span className="text-[#8D8B8B]">Made for everyone</span></b>
-                        <Link href="/products" className="flex items-center gap-2 text-primary-500">
+                        <Link href="/products" className="flex items-center gap-2 text-primary-500 text-lg">
                             See all Vape kits <FaArrowRight />
                         </Link>
                     </div>
@@ -44,7 +44,7 @@ export default function Products() {
                 <div className="w-full">
                     <div className="pr-16 flex justify-between items-center mb-8">
                         <b className="text-3xl font-black">Dispensables - <span className="text-[#8D8B8B]">Perfect for any ocasion</span></b>
-                        <Link href="/products" className="flex items-center gap-2 text-primary-500">
+                        <Link href="/products" className="flex items-center gap-2 text-primary-500 text-lg">
                             See all Dispensables <FaArrowRight />
                         </Link>
                     </div>
