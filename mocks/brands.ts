f61fb4cbaf31ce9f4d78d599399218c8d7f90050
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-18 19:33:00 -0300

    Add brands mock data

---
+++ mocks/brands.ts
@@ -0,0 +1,76 @@
+import { Brands } from "@/interfaces/interfaces";
+import dinnerLadyImage from '@/public/assets/images/mocks/brands/dinner_lady.png';
+import vaporessoImage from '@/public/assets/images/mocks/brands/vaporesso.png';
+import geekVapeImage from '@/public/assets/images/mocks/brands/geekvape.png';
+import smokImage from '@/public/assets/images/mocks/brands/smok.png';
+import fumeImage from '@/public/assets/images/mocks/brands/fume.png';
+import kingCrestImage from '@/public/assets/images/mocks/brands/king_crest.png';
+import vozolImage from '@/public/assets/images/mocks/brands/vozol.png';
+import ijoyImage from '@/public/assets/images/mocks/brands/ijoy.png';
+import twistImage from '@/public/assets/images/mocks/brands/twist.png';
+import joyetechImage from '@/public/assets/images/mocks/brands/joyetech.png';
+import voopoImage from '@/public/assets/images/mocks/brands/voopo.png';
+import humbleImage from '@/public/assets/images/mocks/brands/humble.png';
+
+export const brandsMock: Brands[] = [
+    {
+        id: 1,
+        name: 'Dinner Lady',
+        image: dinnerLadyImage
+    },
+    {
+        id: 2,
+        name: 'Vaporesso',
+        image: vaporessoImage
+    },
+    {
+        id: 3,
+        name: 'Geek Vape',
+        image: geekVapeImage
+    },
+    {
+        id: 4,
+        name: 'Smok',
+        image: smokImage
+    },
+    {
+        id: 5,
+        name: 'Fume Vapes Orjoy',
+        image: fumeImage
+    },
+    {
+        id: 6,
+        name: 'King Crest',
+        image: kingCrestImage
+    },
+    {
+        id: 7,
+        name: 'Vozol',
+        image: vozolImage
+    },
+    {
+        id: 8,
+        name: 'Ijoy',
+        image: ijoyImage
+    },
+    {
+        id: 9,
+        name: 'Twist E-Liquids',
+        image: twistImage
+    },
+    {
+        id: 10,
+        name: 'Joyetech',
+        image: joyetechImage
+    },
+    {
+        id: 11,
+        name: 'Voopo',
+        image: voopoImage
+    },
+    {
+        id: 12,
+        name: 'Humble',
+        image: humbleImage
+    }
+];
