0163bd69fdc304bbdad66f668e130535f592befc
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-18 23:26:06 -0300

    Update styling for category container

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -10,7 +10,7 @@ export default function Categories({ categories }: CategoriesProps) {
 
     return (
         <div>
-            <div className="max-lg:pt-12 max-xl:px-12 mx-auto flex items-center flex-wrap child:flex-grow child:min-w-[300px] justify-center max-w-7xl child:basis-[calc(50%-2rem)] child:relative child:h-72 gap-[2rem]">
+            <div className="max-lg:pt-12 max-xl:px-3 mx-auto flex items-center flex-wrap child:flex-grow child:min-w-[300px] justify-center max-w-7xl child:basis-[calc(50%-2rem)] child:relative child:h-72 gap-[2rem]">
                 {
                     categories.slice(0, 5).map((category, index) => {
                         return (
