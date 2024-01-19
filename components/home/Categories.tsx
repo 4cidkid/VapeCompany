15e0c5e4b48e285f5c2d05061fb8602b2e5cfaad
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-18 23:22:14 -0300

    Refactor category layout in Categories component

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -10,14 +10,14 @@ export default function Categories({ categories }: CategoriesProps) {
 
     return (
         <div>
-            <div className="mx-auto flex items-center flex-wrap justify-center max-w-7xl child:basis-[calc(50%-2rem)] child:relative child:h-72 gap-[2rem]">
+            <div className="max-lg:pt-12 max-xl:px-12 mx-auto flex items-center flex-wrap child:flex-grow child:min-w-[300px] justify-center max-w-7xl child:basis-[calc(50%-2rem)] child:relative child:h-72 gap-[2rem]">
                 {
                     categories.slice(0, 5).map((category, index) => {
                         return (
                             <div key={index} className={`${styles.category} flex-grow`}>
                                 <Image className="absolute left-0 top-0 w-full h-full object-cover object-center" src={category.image} alt={category.name} width={1000} height={1000} />
                                 <div className="w-full h-full flex items-center justify-center backdrop-brightness-[50%]">
-                                    <p className="text-3xl absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 font-bold">
+                                    <p className="max-md:text-xl text-center text-3xl absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 font-bold">
                                         {
                                             category.name
                                         }
