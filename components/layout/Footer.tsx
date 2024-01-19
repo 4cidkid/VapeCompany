9c59303be3bf44b5f86dae51da82dac95be40e1c
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-18 23:22:43 -0300

    Update Footer layout for responsive design

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -7,18 +7,18 @@ export default function Footer() {
     return (
         <footer className="bg-primary-500">
             <div className="p-7">
-                <div className="w-full flex items-center justify-between">
-                    <Logo className="text-4xl" />
+                <div className="max-md:text-left max-md:child:w-fit max-md:gap-3 max-md:items-start max-md:flex-col w-full flex md:items-center md:justify-between">
+                    <Logo className="text-4xl max-md:text-2xl" />
                     <div className="flex items-center gap-7 child:cursor-pointer">
                         <Image width={31} height={31} src={facebookImage} alt="facebook" />
                         <Image width={31} height={31} src={instagramImage} alt="instagram" />
                         <Image width={31} height={31} src={xImage} alt="x" />
                     </div>
                 </div>
-                <div className="pt-4 pb-5 flex items-center justify-between text-white">
+                <div className="max-md:pt-8 pt-4 pb-5 flex flex-wrap child:flex-grow gap-4 child:min-w-[200px] items-center justify-between text-white">
                     <div className="flex flex-col gap-2">
-                        <b className="text-xl">Customer service</b>
-                        <ul className="child:font-light">
+                        <b className="text-xl max-md:text-lg">Customer service</b>
+                        <ul className="child:font-light max-md:text-sm">
                             <li>Contact us</li>
                             <li>Payment</li>
                             <li>Delivery information</li>
@@ -26,8 +26,8 @@ export default function Footer() {
                         </ul>
                     </div>
                     <div className="flex flex-col gap-2">
-                        <b className="text-xl">About us</b>
-                        <ul className="child:font-light">
+                        <b className="text-xl max-md:text-lg">About us</b>
+                        <ul className="child:font-light max-md:text-sm">
                             <li>About us</li>
                             <li>Customer reviews</li>
                             <li>Careers</li>
@@ -35,16 +35,16 @@ export default function Footer() {
                         </ul>
                     </div>
                     <div className="flex flex-col gap-2">
-                        <b className="text-xl">Vaping</b>
-                        <ul className="child:font-light">
+                        <b className="text-xl max-md:text-lg">Vaping</b>
+                        <ul className="child:font-light max-md:text-sm">
                             <li>Blog</li>
                             <li>All products</li>
                             <li>FAQ</li>
                         </ul>
                     </div>
                     <div className="flex flex-col gap-2">
-                        <b className="text-xl">Legal</b>
-                        <ul className="child:font-light">
+                        <b className="text-xl max-md:text-lg">Legal</b>
+                        <ul className="child:font-light max-md:text-sm">
                             <li>Age verification</li>
                             <li>Terms & Conditions</li>
                             <li>Privacy policy</li>
@@ -53,7 +53,7 @@ export default function Footer() {
                     </div>
                 </div>
             </div>
-            <div className="w-full py-3 px-6 border-t border-white text-white">
+            <div className=" w-full py-3 px-6 border-t border-white text-white max-md:text-sm">
                 <span>&copy;{new Date().getFullYear()} VapeCompany All Rights Reserved</span>
             </div>
         </footer>
