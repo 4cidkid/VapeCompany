4c6ffba80db8e53bb92703bb5b975f24da00be45
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-18 23:22:34 -0300

    Refactor newsletter component layout

--- components/home/newsLetter.tsx
+++ components/home/newsLetter.tsx
@@ -9,8 +9,10 @@ export default function Newsletter() {
     }
     return (
         <>
-            <input value={email} onChange={handleEmailChange} type='email' placeholder='Enter your email address' className='rounded-md py-2 pl-3 w-4/5' />
-            <button className='w-1/5 rounded-md bg-white text-primary-500 hover:-translate-y-1 hover:bg-gray-100 transition-all duration-100'>Subscribe</button>
+            <div className="flex items-stretch flex-wrap child:flex-grow gap-2 w-full">
+                <input value={email} onChange={handleEmailChange} type='email' placeholder='Enter your email address' className='basis-[calc(80%-0.5rem)] rounded-md py-2 pl-3 w-4/5' />
+                <button className='py-2 min-w-[100px] basis-1/5 rounded-md bg-white text-primary-500 hover:-translate-y-1 hover:bg-gray-100 transition-all duration-100 max-md:text-sm'>Subscribe</button>
+            </div>
         </>
     )
 }
