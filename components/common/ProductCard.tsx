916dfdab5f1c8dc141c42904e02a1f28be041970
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-18 21:21:05 -0300

    added class to not select images of carousels

--- components/common/ProductCard.tsx
+++ components/common/ProductCard.tsx
@@ -1,11 +1,10 @@
 import { Product } from "@/interfaces/interfaces";
 import Image from "next/image";
-import styles from './productCard.module.css';
 export default function ProductCard({ product }: { product: Product }) {
     return (
         <div className="flex flex-col items-center text-center p-3 border border-[#CCCCCC] bg-white rounded-[33px] w-full h-full max-w-[370px] shadow-[0px_4px_4px_0px_rgba(170,170,170,0.25)]" >
             <div className="w-full aspect-[0.8152] h-auto">
-                <Image className={`w-full h-full object-contain ${styles.productImage}`} src={product.image} alt={product.name} width={200} height={200} />
+                <Image className={`w-full h-full object-contain drag-select-none`} src={product.image} alt={product.name} width={200} height={200} />
             </div>
             <div>
                 <p className="text-xl">{
