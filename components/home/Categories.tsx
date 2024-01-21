7e3d3fffc52c254a3d3354c87bdd41128be277bf
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-21 13:07:08 -0300

    Refactor category display and add button

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -14,14 +14,19 @@ export default function Categories({ categories }: CategoriesProps) {
                 {
                     categories.slice(0, 5).map((category, index) => {
                         return (
-                            <div key={index} className={`${styles.category} flex-grow`}>
+                            <div key={index} className={`${styles.category} flex-grow max-sm:rounded-xl`}>
                                 <Image className="absolute left-0 top-0 w-full h-full object-cover object-center" src={category.image} alt={category.name} width={1000} height={1000} />
                                 <div className="w-full h-full flex items-center justify-center backdrop-brightness-[50%]">
-                                    <p className="max-md:text-xl text-center text-3xl absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 font-bold">
-                                        {
-                                            category.name
-                                        }
-                                    </p>
+                                    <div className="absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 flex flex-col gap-4">
+                                        <p className="max-md:text-xl text-center text-3xl font-bold">
+                                            {
+                                                category.name
+                                            }
+                                        </p>
+                                        <button className="rounded-full whitespace-nowrap flex items-center gap-3 bg-primary-500 px-12 py-2 text-white">
+                                            See {category.name} <FaArrowRight />
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
                         )
