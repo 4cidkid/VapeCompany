856a6f87b6ac4ebba7514c1c22fb472cf27818e5
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-21 14:35:03 -0300

    added new design to categories

--- components/home/Categories.tsx
+++ components/home/Categories.tsx
@@ -10,21 +10,21 @@ export default function Categories({ categories }: CategoriesProps) {
 
     return (
         <div>
-            <div className="max-lg:pt-12 max-xl:px-3 mx-auto flex items-center flex-wrap child:flex-grow child:min-w-[300px] justify-center max-w-7xl child:basis-[calc(50%-2rem)] child:relative child:h-72 gap-[2rem]">
+            <div id={styles.categoryContainer} className="max-lg:pt-12 max-xl:px-3 mx-auto justify-center w-full child:relative child:rounded-xl px-16 child:h-full gap-[2rem]">
                 {
-                    categories.slice(0, 5).map((category, index) => {
+                    categories.slice(0, 10).map((category, index) => {
                         return (
-                            <div key={index} className={`${styles.category} flex-grow max-sm:rounded-xl`}>
+                            <div key={index} className={`${index} w-full ${styles.category} ${index === 1 ? styles.categoryOne : index === 3 ? styles.categoryThree: index === 7 ? styles.categoryThree:index=== 9 ? styles.categoryNine: index === 0 ? styles.categoryCero : index === 8 ? styles.categoryEight: ''}`}>
                                 <Image className="absolute left-0 top-0 w-full h-full object-cover object-center" src={category.image} alt={category.name} width={1000} height={1000} />
-                                <div className="w-full h-full flex items-center justify-center backdrop-brightness-[50%]">
-                                    <div className="absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 flex flex-col gap-4">
+                                <div className="w-full h-full flex items-center justify-center backdrop-brightness-[50%] max-sm:backdrop-brightness-[40%]">
+                                    <div className="p-1 w-full absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 flex flex-col gap-2">
                                         <p className="max-md:text-xl text-center text-3xl font-bold">
                                             {
                                                 category.name
                                             }
                                         </p>
-                                        <button className="rounded-full whitespace-nowrap flex items-center gap-3 bg-primary-500 px-12 py-2 text-white">
-                                            See {category.name} <FaArrowRight />
+                                        <button className="max-sm:p-0 max-sm:w-full max-sm:text-primary-300 max-sm:underline max-sm:font-bold rounded-full flex-wrap justify-center flex items-center gap-2 sm:bg-primary-500 w-[80%] mx-auto max-w-[260px] py-2 text-white">
+                                            <span>See {category.name}</span> <FaArrowRight className="max-sm:hidden" />
                                         </button>
                                     </div>
                                 </div>
@@ -33,6 +33,6 @@ export default function Categories({ categories }: CategoriesProps) {
                     })
                 }
             </div>
-        </div>
+        </div >
     )
 }
