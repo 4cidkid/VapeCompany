d0cd44d9357647b18e690b9945801fc31b90222c
Nicolas Torres <desmondgamesyt@gmail.com>
2024-01-21 14:45:19 -0300

    Update HeroSection component styling

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -9,7 +9,7 @@ export default function HeroSection() {
     )
     return (
         <>
-            <section className="w-full h-[650px] bg-no-repeat bg-right bg-cover max-sm:bg-center max-sm:bg-[url('https://cdn.nicotordev.com/files/7c32ac19-e9e3-4852-a14b-4aca25d57e9a.webp')] max-lg:bg-[url('https://cdn.nicotordev.com/files/8e16a671-4a6a-4293-9b3b-10d5a221929b.webp')] bg-[url('https://cdn.nicotordev.com/files/80a2adb6-6318-4b95-a5f3-e0038d3d08d8.webp')]">
+            <section className="w-full h-[650px] max-sm:h-[550px] bg-no-repeat bg-right bg-cover max-sm:bg-center max-sm:bg-[url('https://cdn.nicotordev.com/files/7c32ac19-e9e3-4852-a14b-4aca25d57e9a.webp')] max-lg:bg-[url('https://cdn.nicotordev.com/files/8e16a671-4a6a-4293-9b3b-10d5a221929b.webp')] bg-[url('https://cdn.nicotordev.com/files/80a2adb6-6318-4b95-a5f3-e0038d3d08d8.webp')]">
                 <div className="relative z-10 max-sm:px-0 max-lg:px-5  max-lg:backdrop-brightness-[35%] w-full h-full px-24 flex items-center justify-start max-lg:justify-center">
                     <div className="text-white mb-20 max-lg:flex max-lg:flex-col max-lg:items-center max-lg:text-center">
                         <h2 className="uppercase font-black text-white text-6xl max-md:text-4xl">VAPORESSO<br /> TAROT NANO</h2>
